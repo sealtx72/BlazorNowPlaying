ddeca08df707ca9f6259311dddd31bc4125a1944
sealtx72 <84211791+sealtx72@users.noreply.github.com>
2026-03-12 13:06:59 -0400

    completed project

--- Components/UI/ActorSwiper/ActorSwiper.razor.js
+++ Components/UI/ActorSwiper/ActorSwiper.razor.js
@@ -1,1 +1,48 @@
-﻿
+﻿export function init(container) {
+    container.querySelector('.nav-btn-next').addEventListener('click', () => {
+        goToNext(container);
+    });
+
+    container.querySelector('.nav-btn-prev').addEventListener('click', () => {
+        goToPrev(container);
+    });
+}
+
+function goToNext(container)
+{
+    const btn = container.querySelector('.nav-btn-prev');
+    const btnWidth = btn.getBoundingClientRect().right;
+
+    const contentArea = container.querySelector('.swiper-content');
+
+    const items = contentArea.querySelectorAll('.swiper-item');
+
+    for (let i = 0; i < items.length; i++) {
+        const start = Math.floor(items[i].getBoundingClientRect().left);
+
+        if (start - btnWidth > 5) {
+            contentArea.scrollLeft += start - btnWidth;
+            break;
+        };
+    }
+
+}
+
+function goToPrev(container)
+{
+    const btn = container.querySelector('.nav-btn-prev');
+    const btnWidth = btn.getBoundingClientRect().right;
+
+    const contentArea = container.querySelector('.swiper-content');
+
+    const items = contentArea.querySelectorAll('.swiper-item');
+
+    for (let i = items.length - 1; i >= 0; i--) {
+        const start = Math.ceil(items[i].getBoundingClientRect().left);
+
+        if (start < (btnWidth - 5)) {
+            contentArea.scrollLeft += start - btnWidth;
+            break;
+        };
+    }
+}
